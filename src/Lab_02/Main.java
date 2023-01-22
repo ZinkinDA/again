9c6f8903eae51721d884d5c7597dd096fd0eb409
Dmitry Zinkin <yashtlsht@mail.ru>
2023-01-22 19:07:06 +0300

    Lab_02 + update

--- src/Lab_02/Main.java
+++ src/Lab_02/Main.java
@@ -2,6 +2,7 @@ package Lab_02;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -23,11 +24,15 @@ public class Main {
 
         scanner = new Scanner(file);
         Integer[][] matrix = new Integer[LJ][LI];// Создаем массив матрицы
-
-        for (int i = 0; i < LJ; i++) {
-            for (int j = 0; j < LI; j++) {
-                matrix[i][j] = scanner.nextInt(); // Записываем элементы
+        try {
+            for (int i = 0; i < LJ; i++) {
+                for (int j = 0; j < LI; j++) {
+                    matrix[i][j] = scanner.nextInt(); // Записываем элементы
+                }
             }
+        } catch (InputMismatchException e){
+            System.err.println("Проверьте данные в файле");
+            System.exit(1);
         }
         //Cчитываем теперь с консоли
         scanner = new Scanner(System.in);
@@ -36,14 +41,21 @@ public class Main {
         System.out.println(spaceMatrix.printSpaceMatrix());//Вывод этой же разреженной матрицы
 
         //ЛР
-        System.out.println("Задание лабораторной работы (вариант 6)");
+        System.out.println("Задание лабораторной работы (вариант 11)");
         System.out.print("Введите число B : ");
-        int b = scanner.nextInt();
-        SpaceMatrix result = spaceMatrix.labsMethod(b); // Все реализация решения задачи
+        try {
 
-        System.out.println(result.printMatrix()); //Вывод матрицы
-        System.out.println(result.printSpaceMatrix());//Вывод этой же разреженной матрицы
+            int b = scanner.nextInt();
+            SpaceMatrix result = spaceMatrix.labsMethod(b); // Все реализация решения задачи
 
-        scanner.close();// закрываем сканер
+            System.out.println(result.printMatrix()); //Вывод матрицы
+            System.out.println(result.printSpaceMatrix());//Вывод этой же разреженной матрицы
+
+            scanner.close();// закрываем сканер
+
+        } catch (InputMismatchException e){
+            System.err.println("Вы ввели с консоли не число!");
+            System.exit(1);
+        }
     }
 }
